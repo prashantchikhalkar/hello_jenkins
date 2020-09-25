e057334cef6db1ad00e7dc2591bef9e6a1c23ee2
prashant <prashantchikhalkar@gmail.com>
2020-09-25 18:00:02 +0530

    Create hello.c - Simple hello world app in C.

---
+++ hello/hello.c
@@ -0,0 +1,13 @@
+/*
+ * hello.c - Simple hello world app in C.
+ */
+#include <stdio.h>
+
+int main (void) {
+  printf ("Hello, World\n");
+  printf ("Hello, Jenkins\n");
+  
+  printf ("Demo hello app in C using Jenkins\n");
+  
+  return 0;
+}
